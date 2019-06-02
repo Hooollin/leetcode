8dbad949316a269e39d7bdb94cc2df625ffa5ea5
回头太难 <lan-cin@qq.com>
2019-06-02 19:47:23 +0800

    6/2/2019

---
+++ easy/349.intersection-of-two-arrays.java
@@ -0,0 +1,68 @@
+
+/*
+ * @lc app=leetcode id=349 lang=java
+ *
+ * [349] Intersection of Two Arrays
+ *
+ * https://leetcode.com/problems/intersection-of-two-arrays/description/
+ *
+ * algorithms
+ * Easy (54.65%)
+ * Likes:    399
+ * Dislikes: 795
+ * Total Accepted:    220.4K
+ * Total Submissions: 403.1K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * Given two arrays, write a function to compute their intersection.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2]
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [9,4]
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * Each element in the result must be unique.
+ * The result can be in any order.
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int p1 = 0, p2 = 0;
+        List<Integer> res = new ArrayList<>();
+        while (p1 < nums1.length && p2 < nums2.length) {
+            if (nums1[p1] == nums2[p2]) {
+                if (!res.contains(nums1[p1]))
+                    res.add(nums1[p1]);
+                p1 += 1;
+                p2 += 1;
+            } else if (nums1[p1] > nums2[p2]) {
+                p2 += 1;
+            } else {
+                p1 += 1;
+            }
+        }
+        int[] arr = new int[res.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = res.get(i);
+        }
+        return arr;
+    }
+}
