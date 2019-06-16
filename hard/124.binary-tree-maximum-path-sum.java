73b916eec704afc1a0c70df6ad5daf92268ec3a8
回头太难 <lan-cin@qq.com>
2019-06-16 11:58:46 +0800

    6/16/2019

---
+++ hard/124.binary-tree-maximum-path-sum.java
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=124 lang=java
+ *
+ * [124] Binary Tree Maximum Path Sum
+ *
+ * https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
+ *
+ * algorithms
+ * Hard (30.10%)
+ * Likes:    1628
+ * Dislikes: 119
+ * Total Accepted:    195.9K
+ * Total Submissions: 649.7K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a non-empty binary tree, find the maximum path sum.
+ * 
+ * For this problem, a path is defined as any sequence of nodes from some
+ * starting node to any node in the tree along the parent-child connections.
+ * The path must contain at least one node and does not need to go through the
+ * root.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3]
+ * 
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   3
+ * 
+ * Output: 6
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [-10,9,20,null,null,15,7]
+ * 
+ * -10
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 
+ * Output: 42
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    int maxValue = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return maxValue;
+    }
+
+    public int helper(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+        maxValue = Math.max(left + right + root.val, maxValue);
+        return root.val + Math.max(left, right);
+    }
+}
