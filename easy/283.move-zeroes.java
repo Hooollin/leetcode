587540b99f6d80897a61f6e4c3ad6a1b8ae764af
回头太难 <lan-cin@qq.com>
2019-07-12 23:03:10 +0800

    7/12/2019

---
+++ easy/283.move-zeroes.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=283 lang=java
+ *
+ * [283] Move Zeroes
+ *
+ * https://leetcode.com/problems/move-zeroes/description/
+ *
+ * algorithms
+ * Easy (54.39%)
+ * Likes:    2136
+ * Dislikes: 79
+ * Total Accepted:    486.9K
+ * Total Submissions: 891.2K
+ * Testcase Example:  '[0,1,0,3,12]'
+ *
+ * Given an array nums, write a function to move all 0's to the end of it while
+ * maintaining the relative order of the non-zero elements.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * 
+ * Note:
+ * 
+ * 
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ * */
+class Solution {
+    // 一开始用的bubble sort，看答案过后改成了o(n)的这个算法
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        for (int num : nums) {
+            if (num != 0)
+                nums[insertPos++] = num;
+        }
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+}
